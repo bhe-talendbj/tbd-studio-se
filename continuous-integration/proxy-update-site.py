5b823e3ce24dfd9813653c53ca6f088f4071be61
Jean-Philippe HAUTIN <jphautin@talend.com>
2019-10-25 16:38:57 +0200

    feat(chore): add jenkins quick build for tbd-studio-se only

--- continuous-integration/proxy-update-site.py
+++ continuous-integration/proxy-update-site.py
@@ -9,21 +9,23 @@ import time
 import threading
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import socket
+from xml.etree import ElementTree
 
-NEXUS_DOMAIN = 'artifacts-zl.talend.com'
-NEXUS_REPOSITORY = 'https://' + NEXUS_DOMAIN + '/nexus/content/unzip/TalendP2UnzipSnapshot/org/talend/studio/talend-tup-p2-repo/7.3.1-SNAPSHOT/talend-tup-p2-repo-7.3.1-SNAPSHOT.zip-unzip'
+NAMESPACE = '{http://maven.apache.org/POM/4.0.0}'
+VERSION = NAMESPACE + 'parent' + '/' + NAMESPACE + 'version'
 
 
 def install_opener(nexus_server, login, password, verify_certificate=False):
     https_handler = None
     auth_handler = None
-    if NEXUS_REPOSITORY.startswith("https"):
+    if proxied_url.startswith('https'):
         context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         if verify_certificate:
             context.verify_mode = ssl.CERT_REQUIRED
         https_handler = urllib.request.HTTPSHandler(context=context)
     if login is not None and password is not None:
         manager = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+        print("installing authentication for user %s on domain %s" % (login, nexus_server))
         manager.add_password(None, nexus_server, login, password)
         auth_handler = urllib.request.HTTPBasicAuthHandler(manager)
     if https_handler is not None:
@@ -55,7 +57,7 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
         """
         :return: the http response of nexus
         """
-        url = NEXUS_REPOSITORY + self.path
+        url = proxied_url + self.path
         request = urllib.request.Request(url, method=method, headers=self.headers)
         print("proxying %s" % url)
         try:
@@ -92,8 +94,8 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
 
 class HttpServerMain:
 
-    def __init__(self, nexus_user=None, nexus_password=None, port=8080, nexus_server=NEXUS_DOMAIN):
-        self.server = HTTPServer(('0.0.0.0', port), ProxyHTTPRequestHandler)
+    def __init__(self, port=8080, binding='0.0.0.0'):
+        self.server = HTTPServer((binding, port), ProxyHTTPRequestHandler)
         self._continue = True
         self.server.timeout = 30
 
@@ -119,21 +121,51 @@ def update_target_platform(port):
         for line in template_file.readlines():
             target_file.write(
                 line if '<repository location="http://localhost:8080/"' not in line else (
-                            '			<repository location="%s/"/>\n' % url))
+                        '			<repository location="%s/"/>\n' % url))
+
+
+def read_pom_version():
+    """
+    quick parse of pom file to find the project version
+    :return: declared version
+    """
+    root = ElementTree.parse('pom.xml').getroot()
+    return root.findall(VERSION)[0].text
+
+
+def get_repository_protocol(unsecured_protocol):
+    return 'http' if unsecured_protocol else 'https'
+
+
+def get_repository_path(pattern, version):
+    if version is None:
+        version = read_pom_version()
+    return pattern.replace('$version', version)
 
 
 if __name__ == '__main__':
+    global proxied_url
     parser = argparse.ArgumentParser(description='start an static http server until a file is deleted')
     parser.add_argument('file', type=str, help='file to check for liveness')
-    parser.add_argument('--nexus-url', '-s', type=str, help='nexus server url')
+    parser.add_argument('--nexus-domain', '-d', type=str, help='nexus server domain', default='artifacts-zl.talend.com')
+    parser.add_argument('--nexus-repository-pattern', type=str, help='nexus server domain',
+                        default='/nexus/content/unzip/TalendP2UnzipSnapshot/org/talend/studio/talend-tup-p2-repo/$version/talend-tup-p2-repo-$version.zip-unzip')
+    parser.add_argument('--nexus-repository-unsecured', '-s', action='store_true',
+                        help='is proxied repository is unsecured', default=False)
+    parser.add_argument('--nexus-repository-version', '-v', type=str, help='is proxied repository is unsecured',
+                        default=None)
     parser.add_argument('--nexus-user', '-u', type=str, help='nexus user')
     parser.add_argument('--nexus-password', '-p', type=str, help='nexus password')
     parser.add_argument('--listening-port', '-l', type=int, default=8080, help='port of http proxy server')
     args = parser.parse_args()
     update_target_platform(args.listening_port)
     print("server is checking for liveness file %s" % args.file)
-    install_opener(NEXUS_DOMAIN, args.nexus_user, args.nexus_password)
-    httpServerMain = HttpServerMain(args.nexus_user, args.nexus_password, args.listening_port)
+    protocol = get_repository_protocol(args.nexus_repository_unsecured)
+    path = get_repository_path(args.nexus_repository_pattern, args.nexus_repository_version)
+
+    proxied_url = "%s://%s%s" % (protocol, args.nexus_domain, path)
+    install_opener(args.nexus_domain, args.nexus_user, args.nexus_password)
+    httpServerMain = HttpServerMain(args.listening_port)
     httpserverThread = threading.Thread(target=httpServerMain.run)
     httpserverThread.start()
     try:
