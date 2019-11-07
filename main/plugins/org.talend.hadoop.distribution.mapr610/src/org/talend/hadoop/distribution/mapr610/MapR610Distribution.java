45c1df2744762bab5c5778c45e0fd0be8fd368a8
Andrii Medvedenko <amedvedenko@talend.com>
2019-11-07 19:02:35 +0200

    TBD-9230/TBD-9229: reverting fix to test changes

--- main/plugins/org.talend.hadoop.distribution.mapr610/src/org/talend/hadoop/distribution/mapr610/MapR610Distribution.java
+++ main/plugins/org.talend.hadoop.distribution.mapr610/src/org/talend/hadoop/distribution/mapr610/MapR610Distribution.java
@@ -492,14 +492,4 @@ public class MapR610Distribution extends AbstractMapRDistribution implements HDF
     public boolean doSupportStoreAsParquet() {
         return false;
     }
-
-    @Override
-    public boolean useOldAWSAPI() {
-        return false;
-    }
-
-    @Override
-    public boolean useS3AProperties() {
-        return true;
-    }
 }
